6b3c6d2ca553f252a32614cd608e52bc70da495b
Safia Saci <safia.saci@inrae.fr>
2021-03-05 15:53:28 +0100

    add r function

--- R/global_plot.R
+++ R/global_plot.R
@@ -17,8 +17,8 @@
 global_plot <- function(x, mycols = paste0("window_", 1:51)){
     graphics::layout(matrix(1:4, ncol = 2, byrow = TRUE), heights = c(0.8, 0.2))
     graphics::par(mar=c(2.4,2.4,1.8,1.1))
-    plotExpressionProfile(x$exp, title = "Expression")
-    plotStackedProfile(x, max_value = 1, title = "DNA methylation")
+    plotExpressionProfile(x$exp)
+    plotStackedProfile(x, max_value = 1)
     graphics::par(mar=c(2.5,2.4,1.5,1.1))
     boxplot_profileExp(x$exp)
     average_profile(x, mycols)
--- tests/testthat/test_plotStackedProfile.R
+++ tests/testthat/test_plotStackedProfile.R
@@ -1,3 +1,9 @@
 context("plotStackedProfile")
 
 
+
+
+plot_test <- plotStackedProfile(gRanges_test,
+                   max_value = 1,
+                   target_height = length(gRanges_test),
+                   title = "DNA methylation")
