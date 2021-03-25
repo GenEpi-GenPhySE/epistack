40004e1f29f3254a30f3908361fdee57d0679161
SafiaSaci <38037077+SaciSafia@users.noreply.github.com>
2021-03-25 21:06:58 +0100

    Add files via upload

---
+++ R/addBins.R
@@ -0,0 +1,31 @@
+
+#' addBins
+#'
+#' @param gr a gRanges object
+#' @param nbins a number corresponding to
+#' @param bin a vector on number
+#'
+#' @return a gRanges with a new "bin" column
+#'
+#' @import rtracklayer
+#'
+#' @examples
+#' data <- transform(data, log2fc_line = as.numeric(log2fc_line))
+#' mat <- data[data$adjpval_line <= 0.01, c("gene", "log2fc_line", "gene_type")]
+#' TSS <- promoters(TSS, upstream = 0, downstream = 1)
+#' my_tss <- addMetricAndArrangeGRanges(TSS, mat, order_key = "gene", order_value = "log2fc_line")
+#' my_tss <- addBins(my_tss)
+#' my_tss
+
+addBins <- function(gr, nbins = 5, bin = NULL){
+
+    if(is.null(bin)) {
+        gr$bin = seq(1, nbins, length.out = length(gr))
+    } else {
+        gr$bin = bin
+    }
+
+    gr
+}
+
+
