3ecca45921703a9213d4236eca19218eccf3479d
SafiaSaci <38037077+SaciSafia@users.noreply.github.com>
2021-03-25 21:19:26 +0100

    Add files via upload

---
+++ R/TSS.R
@@ -0,0 +1,11 @@
+#' a TSS file
+#'
+#' @return A \code{a gRanges object}.
+#'
+#' @format A \code{a gRanges}
+#'
+#'
+#' @source From the GFF3 file provided by GENCODE is generated a .bed file of a single TSS per gene
+#'
+#'
+"TSS"
---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' a metadata file
+#'
+#' @return A \code{a matrix}.
+#'
+#' @format A \code{a csv file} containing 13738 obs of 12 variables
+#'
+#'
+#' @source a csv file containing all the rnaseq
+#'    information of the differentially expressed genes
+#'
+#'
+"data"
