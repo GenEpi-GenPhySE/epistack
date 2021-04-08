dfbb39aff0a728c23d471c47ae290e70d2d9f5d0
SafiaSaci <38037077+SaciSafia@users.noreply.github.com>
2021-04-08 10:18:11 +0200

    Update plotLegendGeneType.R

--- R/plotLegendGeneType.R
+++ R/plotLegendGeneType.R
@@ -4,15 +4,17 @@
 #'
 #' @param gr a gRanges object
 #'
-#' @return
+#' @return a legend for plot of gene_type
+#'
+#' @importFrom grDevices rainbow
 #'
 #' @examples
 #' legned_gene_type(my_tss)
 legned_gene_type <- function(gr){
-    geneTypePalette = rainbow
-    ml <- legend("topright", legend = rep("", length(unique(gr$gene_type))), fill = geneTypePalette(length(unique(gr$gene_type))), bty = "n",
+    geneTypePalette = RColorBrewer::brewer.pal
+    ml <- graphics::legend("topright", legend = rep("", length(unique(gr$gene_type))), fill = geneTypePalette(length(unique(gr$gene_type)), name = "RdBu"), bty = "n",
                  title = "gene type:", title.adj = -8)
-    text(ml$text$x - 0.1, ml$text$y - 0.003, levels(factor(gr$gene_type)), pos = 2)
+    graphics::text(ml$text$x - 0.15, ml$text$y - 0.003, levels(factor(gr$gene_type)), pos = 2)
 }
 
 
