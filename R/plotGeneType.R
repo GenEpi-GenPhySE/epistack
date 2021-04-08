2446ec9ce7bc42ab235b44f0d9e3a2bbd73dca12
SafiaSaci <38037077+SaciSafia@users.noreply.github.com>
2021-04-08 10:16:47 +0200

    Update plotGeneType.R

--- R/plotGeneType.R
+++ R/plotGeneType.R
@@ -5,16 +5,19 @@
 #'
 #' @return a plot
 #'
+#' @importFrom grDevices rainbow
+#'
 #' @examples
 #' plot_gene_type(my_tss)
 #'
 plot_gene_type <- function(gr){
-    geneTypePalette = rainbow
+    geneTypePalette = RColorBrewer::brewer.pal
     geneType <- as.numeric(factor(gr$gene_type))
-    image(t(rev(geneType)), col = geneTypePalette(length(unique(gr$gene_type))),
+    image(t(rev(geneType)), col = geneTypePalette(length(unique(gr$gene_type)), name = "RdBu"),
           axes = FALSE,
           breaks = seq(0.5, length(unique(gr$gene_type)) + 0.5, by = 1))
-    box()
+    graphics::box()
     mtext(side = 3, "gene\ntype", line = 0.5,  cex = 0.8, las = 2)
 
 }
+
