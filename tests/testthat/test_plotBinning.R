cbe39df0d59c0a0cab124b30b12a6d9e0f16dcbb
Guillaume Devailly <gdevailly@hotmail.com>
2021-07-05 17:29:36 +0200

    unit test plotBinning

---
+++ tests/testthat/test_plotBinning.R
@@ -0,0 +1,15 @@
+data("gRanges_test")
+gr <- gRanges_test
+gr <- addBins(gr, nbins = 3)
+plot_bin1 <- function() plotBinning(gr)
+
+gr2 <- data.frame(bin = rep(c(1,2,3,4), each = 5))
+plot_bin2 <- function() plotBinning(gr2, palette = colorRampPalette(c("blue4", "forestgreen", "coral3", "goldenrod")))
+
+test_that("default palette", {
+    vdiffr::expect_doppelganger("plotBin default", plot_bin1)
+})
+
+test_that("custom palette", {
+    vdiffr::expect_doppelganger("plotBin custom", plot_bin2)
+})
