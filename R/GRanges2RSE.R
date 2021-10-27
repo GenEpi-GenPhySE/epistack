8760119567fffd22601a938d08677e20ee19eea4
Guillaume Devailly <gdevailly@hotmail.com>
2021-10-27 17:36:36 +0200

    GR2RSE initial commit

--- R/GRanges2RSE.R
+++ R/GRanges2RSE.R
@@ -1,3 +1,17 @@
-GRanges2RSE <- function(x, patterns) {
-    
+GRanges2RSE <- function(x, patterns, names = patterns) {
+
+    mat <- S4Vectors::mcols(gr)
+
+    assays <- lapply(patterns, function(pattern) {
+        whichCols <- grepl(pattern, colnames(mat))
+        as.matrix(mat[, whichCols])
+    })
+
+
+
+    testdata <- SummarizedExperiment(
+        assays = assays
+        rowData = epidata
+    )
+
 }
